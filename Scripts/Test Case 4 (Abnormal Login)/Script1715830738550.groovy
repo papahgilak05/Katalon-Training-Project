84818ca449aa23e1bbeb78f364948e89dd17356b
papah <papah@PapahGilak>
2024-05-16 14:51:38 +0700

    Update Test Case 1 Normal Login and 4 Abnormal Login (1)

--- Scripts/Test Case 4 (Abnormal Login)/Script1715830738550.groovy
+++ Scripts/Test Case 4 (Abnormal Login)/Script1715830738550.groovy
@@ -17,17 +17,37 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+//-------------------------- Membuka Browser --------------------------------------------//
 WebUI.openBrowser('')
+WebUI.maximizeWindow()
 
+//-------------------------- Membuka Website Metrodata Academy --------------------------//
 WebUI.navigateToUrl('https://metrodataacademy.id/')
+'Berhasil Membuka Website Metrodata Academy'
+WebUI.takeScreenshot()
+//WebUI.click(findTestObject('Object Repository/Page_Metrodata Academy/path'))
 
-WebUI.click(findTestObject('Object Repository/Page_Metrodata Academy/path'))
-
+//-------------------------- Masuk Kedalam Login Page ------------------------------------//
 WebUI.click(findTestObject('Object Repository/Page_Metrodata Academy/a_Masuk'))
 
 WebUI.setText(findTestObject('Object Repository/Page_Metrodata Academy/input__email'), 'papahgilak@gmail.com')
 
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_Metrodata Academy/input__password'), '4VRtK9SyQCROImfHX7yNDFtP+dE0BemFQA7XPsqhpiElUv38N1rW0A==')
+'Melakukan Input Data Login'
+WebUI.takeScreenshot()
 
+WebUI.click(findTestObject('Object Repository/Page_Metrodata Academy/button_Masuk'))
+
+if(WebUI.verifyTextPresent("Try Again", false))
+{
+	'Hasil Sesuai dan Gagal Login'
+	WebUI.takeScreenshot()
+}else {
+	'Hasil tidak sesuai dan Berhasil Login'
+	WebUI.takeScreenshot()
+}
+
+WebUI.delay(5)
 WebUI.click(findTestObject('Object Repository/Page_Metrodata Academy/button_Try Again'))
+WebUI.closeBrowser()
 
