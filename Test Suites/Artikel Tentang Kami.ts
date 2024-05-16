7bcefe5c0a91793b330f83c8e86019582879f7eb
PEGGY BEDSEPTI W <PEGGY BEDSEPTI W@PEGGY-BEDSEPTIW>
2024-05-16 18:22:25 +0700

    Peggy Nambah

---
+++ Test Suites/Artikel Tentang Kami.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Artikel Tentang Kami</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>33db067f-e1a4-46ce-af06-0bae499900b9</testSuiteGuid>
+   <testCaseLink>
+      <guid>3c8da385-8a48-41f6-967a-56038d455d22</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 9 (Normal Artikel dan Tentang Kami)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -18,4 +18,32 @@
       <testCaseId>Test Cases/Test Case 1 (Normal Login)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
+   <testCaseLink>
+      <guid>e65c665b-a088-483b-82d8-0921f825f6be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 10 (Abnormal Login Lupa Password)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77f6c79f-7f67-46ce-a829-d94f1be9941c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 11 (Abnormal Login Validasi Field)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d3aa0121-d547-4fa1-ac34-df577b9ae32c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 4 (Abnormal Login Pass Tidak Sesuai)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>abe010d1-150e-4e51-bda3-09d783e7f5e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 8 (Abnormal Login Email Tidak Sesuai)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
