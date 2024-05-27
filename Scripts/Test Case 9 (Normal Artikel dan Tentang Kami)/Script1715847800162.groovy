9b449565ddf6ef80b815b0c9e59ec8b633e1031d
papah <papahgilak@gmail.com>
2024-05-27 10:21:45 +0700

    Add Last Fixing

--- Scripts/Test Case 9 (Normal Artikel dan Tentang Kami)/Script1715847800162.groovy
+++ Scripts/Test Case 9 (Normal Artikel dan Tentang Kami)/Script1715847800162.groovy
@@ -18,6 +18,7 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
+WebUI.maximizeWindow()
 
 WebUI.navigateToUrl('https://metrodataacademy.id/')
 'Halaman Metrodata'
